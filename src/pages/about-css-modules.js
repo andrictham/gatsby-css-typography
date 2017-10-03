7c64554893f923f7cd236e939bc06aa1b58ff298
Andric Tham <andric@nuisance.co>
2017-10-03 21:41:29 +0800

    Create User component and render it inline

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -2,8 +2,27 @@ import React from 'react'
 import Container from '../components/Container'
 import styles from './about-css-modules.module.css'
 
+const User = ({avatar, username, excerpt}) =>
+  <div>
+    <img src={avatar} alt=""/>
+    <div>
+      <h2>{username}</h2>
+      <p>{excerpt}</p>
+    </div>
+  </div>
+
 export default () =>
   <Container>
     <h1>About CSS Modules</h1>
     <p>CSS Modules are cool!</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
   </Container>
